1cc9ad7e77ce3d81227890816c262b1592dc3f2b
AbhishekM420 <abiz007ip@gmail.com>
2023-02-24 12:44:26 +0530

    final

--- src/home.js
+++ src/home.js
@@ -3,6 +3,7 @@ export function homepage(){
     const content = document.querySelector("#content");
     content.innerHTML= " ";
     const test = document.createElement('div');
+    test.className = "test";
     test.textContent= 'At (RN), we’re serving up more than (TF). In fact, (RN) Famous (recipe) is one of our unexpected specialties. Reminiscent of butcher shops back in the day, each slow-smoked, sizzling prime chop measures seven-fingers high. Our signature recipe, that we have perfected for more than four decades, is rubbed with a secret blend of seasonings, cured and roasted on a rotisserie with pecan wood for up to six hours before it’s topped with (RN) signature herb-garlic butter, then carved tableside.(RN) specializes idelicious food featuring fresh ingredients and masterful preparation by the (RN) culinary team. Whether you’re ordering a multi-course meal or grabbing a drink and pizza at the bar, (RN’s) lively, casual yet upscale atmosphere makes it perfect for dining with friends, family, clients and business associates.he approach to the menu was easy. We had no interest in trying to reinvent food. We went with choices that were popular catering requests — basic, down-home style — just from a wide range of regions. We are known for our generous portions of BBQ, Southern, Cajun/Creole meals, plus a touch of Caribbean fun. We will tell you now – save room for dessert';
     content.appendChild(test);
 
--- src/menu.js
+++ src/menu.js
@@ -8,16 +8,35 @@ export function menuPage(){
     menu.textContent= 'Menu Of Happiness';
     content.appendChild(menu);
 
+
     const menu1 = document.createElement('div');
     menu1.textContent= 'Gobi Tandoori';
+    menu1.className ="menu1";
     menu.appendChild(menu1);
+    const img1 = document.createElement("img");
+    img1.src = "/src/gobi.jpg";
+    img1.setAttribute("height", "250");
+    img1.setAttribute("width", "250");
+    menu1.appendChild(img1);
 
     const menu2 = document.createElement('div');
-    menu.textContent= 'Mushroom Tandoori';
+    menu2.textContent= 'Mushroom Tandoori';
+    menu2.className ="menu2";
     menu.appendChild(menu2);
+    const img2 = document.createElement("img");
+    img2.src = "/src/mush.jpg";
+    img2.setAttribute("height", "250");
+    img2.setAttribute("width", "250");
+    menu2.appendChild(img2);
 
     const menu3 = document.createElement('div');
     menu3.textContent= 'Chaya';
+    menu3.className ="menu3";
     menu.appendChild(menu3);
+    const img3 = document.createElement("img");
+    img3.src = "/src/tea.jpeg";
+    img3.setAttribute("height", "250");
+    img3.setAttribute("width", "250");
+    menu3.appendChild(img3);
 
 }
